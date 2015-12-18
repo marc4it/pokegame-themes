9d22bf66aad1e37776912babe4ca57d19df8cf13
marc4it <marc4it@gmail.com>
2015-12-18 09:36:23 +0000

    moved breadcrumbs to top-bar

--- parts/top-bar.php
+++ parts/top-bar.php
@@ -22,3 +22,60 @@
         </section>
     </nav>
 </div>
+
+<!-- breadcrumbs -->
+<?php
+
+function no_dash($name) {
+	if (strpos($name,'-') !== false) {
+		$new_name = str_replace('-',' ',$name);
+		return $new_name;
+	} else {
+		return $name;
+	}
+}
+
+$urlArr = parse_url(filter_input(INPUT_SERVER, 'REQUEST_URI', FILTER_SANITIZE_STRIPPED));
+$path = explode('/', $urlArr['path']);
+$crumbString1 = "/".$path[1]."/";
+$crumbString2 = "/".$path[1]."/".$path[2]."/";
+
+if ($path[1] == "pokedex" && ($path[2])) {
+	
+	$path2 = $path[2];
+	$sql = "SELECT * FROM `pokedex` WHERE `pokedex_id` LIKE '".$path2."' ;";
+	$results = $wpdb->get_row( $sql, ARRAY_A );
+	
+	echo '<div class="row">
+			<div class="small-12 columns">
+				<ul class="breadcrumbs">
+					<li><a href="/">Home</a></li>
+					<li><a href="'.$crumbString1.'">'.$path[1].'</a></li>
+					<li class="current"><a href="'.$crumbString2.'">'.$results['name'].'</a></li>
+				</ul>
+			</div>
+		</div>';
+		
+} elseif (($path[1]) && ($path[2])) {
+
+		echo '<div class="row">
+				<div class="small-12 columns">
+					<ul class="breadcrumbs">
+						<li><a href="/">Home</a></li>
+						<li><a href="'.$crumbString1.'">'.$path[1].'</a></li>
+						<li class="current"><a href="'.$crumbString2.'">'.no_dash($path[2]).'</a></li>
+					</ul>
+				</div>
+			</div>';
+		
+} elseif (($path[1]) && $path[2] == "") {
+	
+		echo '<div class="row">
+				<div class="small-12 columns">
+					<ul class="breadcrumbs">
+						<li><a href="/">Home</a></li>
+						<li class="current"><a href="'.$crumbString1.'">'.$path[1].'</a></li>
+					</ul>
+				</div>
+			</div>';
+}
